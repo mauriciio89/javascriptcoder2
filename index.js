621b95733a62f634b6457bd56fe6d535dceab021
Mauricio Galvez <maugalvez89@gmail.com>
2023-01-25 23:41:12 -0500

    entrega2

--- index.js
+++ index.js
@@ -1,127 +1,109 @@
-const ingreso = alert("Ingresa tus datos")
-const nombreUsuario = prompt("Cuál es tu nombre?")
-const apellidoUsuario = prompt("Cuál es apellido?")
-
-
-
-let productos = [
+const productos = [
     {
-        nombre : "producto1",
-        precio : 25,
-        
+        nombre: "arroz",
+        precio: 20
     },
     {
-        nombre : "producto2",
-        precio : 20,
-        
+        nombre: "tomate",
+        precio: 10
     },
     {
-        nombre : "producto3",
-        precio : 18,
-        
+        nombre: "queso",
+        precio: 30
     },
     {
-        nombre : "producto4",
-        precio : 16,
-        
+        nombre: "arvejas",
+        precio: 15
     },
-]  
+    {
+        nombre: "atun",
+        precio: 25
+    },
+];
 
+let carrito = []
 
+let seleccion = prompt("Desea algún producto si o no?")
 
-
-const saludo = () => {
-    ingreso
-    alert(`Hola ${nombreUsuario} ${apellidoUsuario} bienvenido a tu sitio web favorito.`)
+while (seleccion != "si" && seleccion != "no") {
+    alert("por favor diga si o no")
+    seleccion = prompt("hola desea comprar algo si o no")
 }
-console.log(saludo)
-saludo()
+
+if (seleccion == "si") {
+    alert("a continuacion nuestra lista de productos")
+    let todosLosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$"
+    );
+    alert(todosLosProductos.join(" - "))
+} else if (seleccion == "no") {
+    alert("gracias por preferirnos , hasta pronto")
+}
 
 
+while (seleccion != "no") {
+    let producto = prompt("agrega un producto a tu carrito")
+    let precio = 0
 
-
- 
-
-
-    
-
-function producto() {
-
-let total = 0
-let nombreProducto = ""
-let cantidad = 0
-let totalDeCompra = 0
-
-let productoSeleccionado = productos.filter(producto => {
-    console.log(producto)
-})
-
-    while(productoSeleccionado !== "6"){
-
-         productoSeleccionado = prompt("Elija una opción 1.producto1 - 2.producto2 - 3.producto3 - 4.producto4 - 5.Compra total -  - 6.Salir")
-        
-         while (isNaN(productoSeleccionado)){
-            productoSeleccionado = prompt("Elija una opción 1.producto1 - 2.producto2 - 3.producto3 - 4.producto4 - 5.Compra total  - 6.Salir Ingrese un número del 1 al 6")
-         }
-        
-        if(productoSeleccionado == "1"){
-        cantidad = parseInt(prompt("Ingrese la cantidad del producto seleccionado."))
-         while(isNaN(cantidad)){
-        cantidad = parseInt(prompt("Ingrese la cantidad del producto seleccionado. (Solo números)"))
-         }   
-        
-        total = producto1*cantidad
-         nombreProducto = "producto1"
-         totalDeCompra = totalDeCompra + total
-
-        }else  if(productoSeleccionado == "2"){
-            cantidad = parseInt(prompt("Ingrese la cantidad del producto seleccionado."))
-            while(isNaN(cantidad)){
-           cantidad = parseInt(prompt("Ingrese la cantidad del producto seleccionado. (Solo números)"))
-            }   
-         total = productos.nombre*cantidad
-         nombreProducto = "producto2"
-         totalDeCompra = totalDeCompra + total
-
-        }else if(productoSeleccionado == "3"){
-            cantidad = parseInt(prompt("Ingrese la cantidad del producto seleccionado."))
-            while(isNaN(cantidad)){
-           cantidad = parseInt(prompt("Ingrese la cantidad del producto seleccionado. (Solo números)"))
-            }   
-         total = producto3*cantidad
-         nombreProducto = "producto3"
-         totalDeCompra = totalDeCompra + total
-
-        }else if(productoSeleccionado == "4"){
-            cantidad = parseInt(prompt("Ingrese la cantidad del producto seleccionado."))
-            while(isNaN(cantidad)){
-           cantidad = parseInt(prompt("Ingrese la cantidad del producto seleccionado. (Solo números)"))
-            }   
-         total = producto4*cantidad
-
-         nombreProducto = "producto4"
-         totalDeCompra = totalDeCompra + total
-
-        }else if(productoSeleccionado == "5"){
-        alert("El valor total de la compra es "+ totalDeCompra)
-           
-        
-        }else if(productoSeleccionado == "6") { 
-            alert("Gracias por comprar")
-        
-        }else { 
-            alert("Ingresaste una opción incorrecta")
+    if (producto == "arroz" || producto == "tomate" || producto == "queso" || producto == "arvejas" || producto == "atun") {
+        switch (producto) {
+            case "arroz":
+                precio = 20;
+                break;
+            case "tomate":
+                precio = 10;
+                break;
+            case "queso":
+                precio = 30;
+                break;
+            case "arvejas":
+                precio = 15;
+                break;
+            case "atun":
+                precio = 25;
+                break;
+            default:
+                break;
         }
-        
-        
-        
-
+      let unidades = parseInt(prompt("Cuantas unidades quiere llevar?"))   
+      
+      carrito.push({producto, unidades, precio})
+      console.log(carrito)
+    } else {
+        alert("no tenemos ese producto")
     }
-   
-    
-   }
 
-producto()
+    seleccion = prompt("desea agregar algo mas?")
+    while(seleccion === "no"){
+        alert("gracias por la compra")
+        carrito.forEach((carritoFinal) => {
+            alert(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+        break;
+    }
+}
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+alert(`El total a pagar por su compra es: ${total}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
